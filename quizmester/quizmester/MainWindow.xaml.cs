309d86ee8766c1a620f8f6b74418dfa266a0175f
pattje72 <91815@roc-teraa.nl>
2025-09-01 20:52:26 +0200

    Add countdown and question screens to quiz flow
    Introduces new countdown and question screens to the quiz application, including timer logic for quiz start and answering questions. Updates UI elements for improved appearance and clarity, refactors high score and quiz creator display, and adds logic to fetch and shuffle quiz questions.

--- quizmester/quizmester/MainWindow.xaml.cs
+++ quizmester/quizmester/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace quizmester
 {
@@ -24,11 +25,29 @@ namespace quizmester
         /// 1 = login screen (Screen1)
         /// 2 = sign up screen (Screen2)
         /// 3 = game choise screen (Screen3)
+        /// 4 = view quiz info screen (Screen4)
+        /// 5 = coundown screen (Screen5)
+        /// 6 = view question screen (Screen6)
         /// default screen is login screen
         /// </summary>
         int CurrentScreen = 1;
         // the max screen number... change this if you add more screens
-        int ScreenMax = 4;
+        int ScreenMax = 6;
+
+        /// <summary>
+        /// countdown timer for the start of the quiz and time left for answering questions
+        /// </summary>
+        int CountDown;
+
+        bool GameStarted = false;
+
+        // timer for the countdown
+        DispatcherTimer timer;
+
+        // the current quiz id
+        int CurrentQuizId = 0;
+
+        List<string> Question_ids = new List<string>();
 
         string connectionString;
 
@@ -153,8 +172,7 @@ namespace quizmester
                             StartupGrid.Visibility = Visibility.Collapsed; 
                             MainGrid.Visibility = Visibility.Visible;
 
-                            if (i >= 5) ;
-                            else if (i >= 3) ShowQuizes();
+                            if (i >= 3 && i < 5) ShowQuizes();
                         }
                         // if the screen is not the game choise screen, make the window normal and show the startup grid
                         else 
@@ -190,10 +208,23 @@ namespace quizmester
             {
                 var border = new Border
                 {
-                    Width = 250, Height = 350,
+                    Width = 250,
+                    Height = 350,
                     Margin = new Thickness(10),
                     CornerRadius = new CornerRadius(20),
-                    Background = Brushes.White
+                    Background = new RadialGradientBrush
+                    {
+                        GradientOrigin = new Point(0.3, 0.3),
+                        RadiusX = 1.2,
+                        RadiusY = 1.2,
+                        GradientStops = new GradientStopCollection
+                            {
+                                new GradientStop((Color)ColorConverter.ConvertFromString("#1A0033"), 0.0),
+                                new GradientStop((Color)ColorConverter.ConvertFromString("#4B0082"), 0.4),
+                                new GradientStop((Color)ColorConverter.ConvertFromString("#6A0DAD"), 0.8),
+                                new GradientStop((Color)ColorConverter.ConvertFromString("#2D1B69"), 1.0),
+                            }
+                    }
                 };
 
                 // create a grid to hold the label and button
@@ -207,9 +238,10 @@ namespace quizmester
                     Content = rowThemes["Theme"].ToString(),
                     HorizontalAlignment = HorizontalAlignment.Center,
                     VerticalAlignment = VerticalAlignment.Top,
-                    FontSize = 24,
+                    FontSize = 32,
                     FontWeight = FontWeights.Bold,
-                    Margin = new Thickness(0, 0, 0, 10)
+                    Margin = new Thickness(0, 0, 0, 10),
+                    Foreground = Brushes.White,
                 });
 
                 // create a button to view the quiz
@@ -239,7 +271,7 @@ namespace quizmester
                 {
                     Orientation = Orientation.Vertical,
                     HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
+                    Width = 200, Height = 225,
                 };
                 grid.Children.Add(NewStackPanel);
 
@@ -253,7 +285,8 @@ namespace quizmester
                         NewStackPanel.Children.Add(new Label
                         {
                             Content = i + ": " + rowScores["Username"].ToString(),
-                            FontSize = 28,
+                            FontSize = 24,
+                            Foreground = Brushes.White,
                         });
                         // go up
                         i++;   
@@ -269,22 +302,66 @@ namespace quizmester
         {
             // find the button that was clicked
             Button button = sender as Button;
-            if (button != null && button.Tag != null)
+            CurrentQuizId = int.Parse(button.Tag.ToString());
+            if (CurrentQuizId != 0)
             {
                 // make the current screen the view screen
                 CurrentScreen = 4;
 
-                int themeId = int.Parse(button.Tag.ToString());
+                // get the quiz id from the button's Tag property
+                int themeId = CurrentQuizId;
+                // get the quiz name from the database from the id in the button's Tag property
                 DataTable DataThemes = Query.GetDataTable("SELECT Account_Id, Theme FROM Themes WHERE Id = '" + themeId + "' ;");
 
+                // go trough all the rows (should only be one)
                 foreach (DataRow rowThemes in DataThemes.Rows)
                 {
                     // set the label to the quiz name
                     var textBlock = this.FindName("LblQuizName") as TextBlock;
                     if (textBlock != null) textBlock.Text = rowThemes["Theme"].ToString();
-                    textBlock = this.FindName("LblQuizCreator") as TextBlock;
-                    if (textBlock != null && rowThemes["Account_Id"] != null ) textBlock.Text = "Created by: " + rowThemes["Account_Id"].ToString();
 
+                    // set the label to the quiz creator
+                    textBlock = this.FindName("LblQuizCreator") as TextBlock;
+                    if (textBlock != null && rowThemes["Account_Id"] != null)
+                    {
+                        // get the username from the database from the account id in the quiz
+                        DataTable DataAcounts = Query.GetDataTable("SELECT Username FROM Accounts WHERE Id = '" + rowThemes["Account_Id"] + "' ;");
+
+                        // go trough all the rows (should only be one)
+                        foreach (DataRow rowAcounts in DataAcounts.Rows)
+                        {
+                            // set the label to the quiz creator
+                            textBlock.Text = "Created by: " + rowAcounts["Username"].ToString();
+                        }
+                    }
+
+                    // get the 10 high scores for this quiz. if there are less than 10,
+                    // it will return all of them,
+                    // if more than 10, it will return the top 10
+                    DataTable dataHighScores = Query.GetDataTable("SELECT TOP 10 Username, Score FROM HighScores WHERE Theme_Id = " + themeId + " ORDER BY Score DESC;");
+
+                    // get the stack panel
+                    var stackPanel = this.FindName("SplAllHighScores") as StackPanel;
+                    if(dataHighScores != null && stackPanel != null)
+                    {
+                        // clear the stack panel
+                        stackPanel.Children.Clear();
+
+                        // to set the place they are in
+                        int i = 1;
+                        foreach (DataRow rowScores in dataHighScores.Rows)
+                        {
+                            // add a label for each high score with the username and score on there place
+                            stackPanel.Children.Add(new Label
+                            {
+                                Content = i + ": " + rowScores["Username"].ToString() + ", Score: " + rowScores["Score"],
+                                FontSize = 24,
+                                VerticalAlignment = VerticalAlignment.Center,
+                                Foreground = Brushes.White
+                            });
+                            i++;
+                        }
+                    }
                 }
             }
 
@@ -315,7 +392,95 @@ namespace quizmester
 
         private void BtnStartQuiz_Click(object sender, RoutedEventArgs e)
         {
+            if(CurrentQuizId != 0)
+            {
+                // go to the CountDown screen
+                CurrentScreen = 5;
+                ScreenCheck();
 
+                LblStartTimer.Visibility = Visibility.Visible;
+
+                // set the countdown time in seconds
+                CountDown = 5;
+                StartTimer();
+            }
+            else { MessageBox.Show("Something went wrong please try again"); CurrentScreen = 3; }
+        }
+
+        private void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // every 1 second
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Safe to update UI here
+            CountDown--;
+            // show the countdown in the label
+            LblStartTimer.Content = CountDown.ToString();
+
+            if (CountDown == 0 & GameStarted == false)
+            {
+                // game starts now
+                GameStarted = true;
+
+                LblStartTimer.Visibility = Visibility.Collapsed;
+
+                // go to the question screen
+                CurrentScreen = 6;
+                ScreenCheck();
+
+                // get and show the questions
+                GetQuestions();
+                ShowQuiz();
+
+                // time to answer the questions
+                CountDown = 60;
+            }
+            else if (CountDown == 0 & GameStarted == true)
+            {
+                // time to answer the question is up
+                timer.Stop();
+                // go to the next question or end the game if there are no more questions
+
+            }
+        }
+        private void GetQuestions()
+        {
+            Question_ids.Clear();
+
+            if (CurrentQuizId != 0)
+            {
+                // get the quiz id from the database from the quiz name
+                DataTable DataThemes = Query.GetDataTable("SELECT Theme FROM Themes WHERE Id = '" + CurrentQuizId + "' ;");
+
+                // go trough all the rows (should only be one)
+                foreach (DataRow rowThemes in DataThemes.Rows)
+                {
+                    lblname.Text = rowThemes["Theme"].ToString();
+                }
+
+                // get the question ids from the database from the quiz id
+                DataTable DataQuestions = Query.GetDataTable("SELECT Id FROM Questions WHERE Theme_Id = '" + CurrentQuizId + "' ;");
+                // go trough all the rows and add the question ids to the list
+                foreach (DataRow rowQuestions in DataQuestions.Rows)
+                {
+                    Question_ids.Add(rowQuestions["Id"].ToString());
+                }
+
+                Question_ids = Question_ids.OrderBy(x => Guid.NewGuid()).ToList(); // shuffle the list
+            }
+        }
+
+        private void ShowQuiz()
+        {
+
+            var stackPanel = this.FindName("SplAnswers") as StackPanel;
+
+            stackPanel.Children.Clear();
         }
     }
 }
